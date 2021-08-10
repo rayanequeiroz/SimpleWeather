36a695ca98337b64a69561841ccf7aa4c6d3163b
evergreen <dany2zverev@gmail.com>
2021-08-11 00:38:53 +0300

    refactoring CityForm to own component

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,39 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import Weather from './components/Weather';
+import CityForm from './components/CityForm';
 
 class WeatherApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: 'Moscow' };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    this.setState({ value: event.target.value });
-    event.preventDefault();
+  handleEvent(event) {
+    this.setState({ value: event.target.value })
   }
 
   render() {
     return (
       <div className="city-form">
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Pick your city:
-            <select value={this.state.value} onChange={this.handleChange}>
-              <option value="Moscow">Moscow</option>
-              <option value="Samara">Samara</option>
-              <option value="Murmansk">Murmansk</option>
-              <option value="Vladivostok">Vladivostok</option>
-            </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+        <CityForm onChange={this.handleEvent} />
         <Weather location={this.state.value} />
       </div>
     );
--- src/components/CityForm.js
+++ src/components/CityForm.js
@@ -1,1 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+const CityForm = (props) => {
+
+  return (
+    <form>
+      <label>
+        Please, pick your city:
+        <select value="Moscow" onChange={(e) => props.onChange(e)}>
+          <option value="Moscow">Moscow</option>
+          <option value="Samara">Samara</option>
+          <option value="Murmansk">Murmansk</option>
+          <option value="Vladivostok">Vladivostok</option>
+        </select>
+      </label>
+    </form>
+  )
+}
+
+export default CityForm;
