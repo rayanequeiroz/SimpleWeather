ca1a7a205a75e888d56b6c6a9af8eae444a60f54
evergreen <dany2zverev@gmail.com>
2021-08-12 21:14:14 +0300

    update search logic

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,27 +4,36 @@ import './Weather.css';
 
 const Weather = (props) => {
   const { location } = props;
-  const [temp, setTemp] = useState('NOT FOUND');
+  const [temp, setTemp] = useState('');
+  const [responseCity, setResponseCity] = useState('');
 
   const loadWeather = async () => {
     try {
       const data = await loadData(location);
       setTemp(data.current.temp_c);
+      setResponseCity(data.location.name);
     } catch (e) {
       console.error(e);
     }
   };
-
   useEffect(() => {
     loadWeather(location);
   }, [loadWeather, location]);
 
-  return (
-    <div className="weather border">
-      <h1>How do you like the weather in {location}?</h1>
-      <h2>It's {temp} C° outside :)</h2>
-    </div>
-  );
+  if (!temp || location !== responseCity) {
+    return (
+      <div></div>
+    );
+  }
+
+  if (temp) {
+    return (
+      <div className="weather border">
+        <h1>How do you like the weather in {location}?</h1>
+        <h2>It's {temp} C° outside :)</h2>
+      </div>
+    );
+  }
 };
 
 export default Weather;
