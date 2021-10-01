0151d87677e28dfe7571c4691b49bb107a370f49
Vadim Usinov <wazzroboy@gmail.com>
2021-10-01 16:31:44 +0300

    add react-routing and url /cities

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { useCallback, useEffect } from 'react';
-import useDebounce from './functions/useDebounce';
-
-import Forecast from './components/Forecast';
-import CitySearch from './components/CitySearch';
-
-import { setCity } from './redux/actions';
-import store from './redux/store';
-import { fetchWeather } from './redux/store';
 import { connect } from 'react-redux';
+import { Route, Router, Switch } from 'react-router-dom';
+import CitiesList from './components/CitiesList';
+import WeatherToday from './components/WeatherToday';
+import useDebounce from './functions/useDebounce';
+import { history } from './historyVar';
+import { setCity } from './redux/actions';
+import store, { fetchWeather } from './redux/store';
+
 
 const App = (props) => {
   useEffect(() => {
-    if (localStorage.location && props.location === "") {
+    if (localStorage.location && props.location === '') {
       store.dispatch(setCity(localStorage.getItem('location')));
       // return () => {
       //   location.onChange(localStorage.setItem('location', location));
       // };
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     return () => {
       localStorage.setItem('location', props.location);
     };
-  }, [])
+  }, []);
 
   const debouncedLocation = useDebounce(props.location.trim(), 700);
 
@@ -35,17 +35,23 @@ const App = (props) => {
     }
   }, [debouncedLocation, memoLoadWeather]);
 
-  return ( 
-      <div className="app flex-container">
-        <CitySearch/>
-        <Forecast/>
-      </div>
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact>
+          <WeatherToday />
+        </Route>
+        <Route path="/cities" exact>
+          <CitiesList />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 
 const mapStateToProps = (state) => {
   const { location } = state;
   return { location };
-}
+};
 export default connect(mapStateToProps)(App);
