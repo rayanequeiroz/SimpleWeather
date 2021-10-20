681989ff912e866977f6553d404885a25b098572
evergreen <dany2zverev@gmail.com>
2021-10-20 21:56:12 +0300

    Changing reducer paths

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import {weatherReducer} from "./weatherReducer";
-import {favoriteCitiesReducer} from "./favoriteCitiesReducer";
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {weatherReducer} from "./weatherReducer/weatherReducer";
+import {favoriteCitiesReducer} from "./favoriteCitiesReducer/favoriteCitiesReducer";
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 
-const rootReducer = combineReducers( {
-  cities: favoriteCitiesReducer,
-  weatherData: weatherReducer
+const rootReducer = combineReducers({
+    cities: favoriteCitiesReducer,
+    weatherData: weatherReducer
 })
 
 let store = createStore(rootReducer, composedEnhancer);
