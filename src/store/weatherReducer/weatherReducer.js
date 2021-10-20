bd9ba83e52624781a633991674e2507989b928b5
evergreen <dany2zverev@gmail.com>
2021-10-20 21:55:34 +0300

    Actions rewritten from functional expressions to arrow functions

--- src/store/weatherReducer/weatherReducer.js
+++ src/store/weatherReducer/weatherReducer.js
@@ -1,41 +1,30 @@
-import fetchData from "../API/fetchData";
-import store from "./store";
+import fetchData from "../../helpers/fetchData";
+import store from "../store";
+import {CHANGE_TEMP_SCALE, SET_CITY, UPDATE_WEATHER} from "./actionTypes";
 
-export const SET_CITY = "SET_CITY";
-export const UPDATE_WEATHER = "UPDATE_WEATHER";
-export const CHANGE_TEMP_SCALE = "CHANGE_TEMP_SCALE";
-
-export function setCity(city) {
-    return { type: SET_CITY, payload: city };
-}
-
-export function updateWeather(response) {
-    return { type: UPDATE_WEATHER, payload: response };
-}
-
-export function changeTempScale(response) {
- return { type: CHANGE_TEMP_SCALE, payload: response };
-}
+export const setCity = (city) => ({type: SET_CITY, payload: city})
+export const updateWeather = (response) => ({type: UPDATE_WEATHER, payload: response})
+export const changeTempScale = (response) => ({type: CHANGE_TEMP_SCALE, payload: response})
 
 export const initialState = {
-    location: "",
-    responseLocation: "",
-    country: "",
-    temp_c: "",
-    temp_f: "",
-    condition: "",
-    wind: "",
-    pressure: "",
-    humidity: "",
-    code: "",
-    isDay: "",
-    tempScale: "celsius"
+    location: '',
+    responseLocation: '',
+    country: '',
+    temp_c: '',
+    temp_f: '',
+    condition: '',
+    wind: '',
+    pressure: '',
+    humidity: '',
+    code: '',
+    isDay: '',
+    tempScale: 'celsius'
 };
 
 export function weatherReducer(state = initialState, action) {
     switch (action.type) {
         case SET_CITY:
-            return { ...state, location: action.payload.toLowerCase() };
+            return {...state, location: action.payload.toLowerCase()};
         case UPDATE_WEATHER:
             return {
                 ...state,
@@ -54,7 +43,7 @@ export function weatherReducer(state = initialState, action) {
             return {
                 ...state,
                 tempScale: action.payload
-            }
+            };
         default:
             return state;
     }
