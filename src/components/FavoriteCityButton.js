5319b7c2de598a0c3646fb9e62828dc3ab6f9362
evergreen <dany2zverev@gmail.com>
2021-10-20 22:15:39 +0300

    Now the city object is sent to the array of cities instead of the city name string

--- src/components/FavoriteCityButton.js
+++ src/components/FavoriteCityButton.js
@@ -1,63 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import FavoriteButton from "./UI/FavoriteButton";
 import {connect} from "react-redux";
-import {addToFavorites, removeFromFavorites} from "../store/favoriteCitiesReducer";
+import {addToFavorites, removeFromFavorites} from "../store/favoriteCitiesReducer/favoriteCitiesReducer";
 
 const FavoriteCityButton = (props) => {
-  const [isFavorite, setFavorite] = useState(
-    props.favoriteCities.includes(props.location)
-  );
-  const [isDisabled, setDisabled] = useState(false);
+    const [isFavorite, setFavorite] = useState(props.favoriteCities.find(obj => obj.city === props.location))
+    const [isDisabled, setDisabled] = useState(false);
 
-  useEffect(() => {
-    setFavorite(props.favoriteCities.includes(props.location));
-  }, [props.location]);
+    useEffect(() => {
+        setFavorite(props.favoriteCities.find(obj => obj.city === props.location));
+    }, [props.location]);
 
-  useEffect(() => {
-    if (
-      props.location &&
-      props.location === props.responseLocation.toLowerCase()
-    ) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-  }, [props.location, props.responseLocation]);
-
-  useEffect(() => {
-    localStorage.setItem("cities", JSON.stringify(props.favoriteCities));
-  }, [props.favoriteCities]);
-
-  return (
-    <FavoriteButton
-      onClick={() => {
-        if (!isFavorite) {
-          props.addToFavorites(props.location);
-          setFavorite(!isFavorite);
+    useEffect(() => {
+        if (
+            props.location &&
+            props.location === props.responseLocation.toLowerCase()
+        ) {
+            setDisabled(false);
         } else {
-          props.removeFromFavorites(props.location);
-          setFavorite(!isFavorite);
+            setDisabled(true);
         }
-      }}
-      disabled={isDisabled}
-      checked={isFavorite}
-    />
-  );
+    }, [props.location, props.responseLocation]);
+
+    useEffect(() => {
+        localStorage.setItem('cities', JSON.stringify(props.favoriteCities));
+    }, [props.favoriteCities]);
+
+    return (
+        <FavoriteButton
+            onClick={() => {
+                if (!isFavorite) {
+                    props.addToFavorites({
+                        city: props.location,
+                        celsius: props.temp_c,
+                        fahrenheit: props.temp_f,
+                        lastUpdated: Date.now()
+                    });
+                    setFavorite(!isFavorite);
+                } else {
+                    props.removeFromFavorites(props.location);
+                    setFavorite(!isFavorite);
+                }
+            }}
+            disabled={isDisabled}
+            checked={!!isFavorite}
+        />
+    );
 };
 
 const mapStateToProps = (state) => ({
-  location: state.weatherData.location,
-  favoriteCities: state.cities.arrOfCities,
-  responseLocation: state.weatherData.responseLocation,
+    location: state.weatherData.location,
+    responseLocation: state.weatherData.responseLocation,
+    temp_c: state.weatherData.temp_c,
+    temp_f: state.weatherData.temp_f,
+    favoriteCities: state.cities.arrOfCities,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  addToFavorites: (city) => {
-    dispatch(addToFavorites(city));
-  },
-  removeFromFavorites: (city) => {
-    dispatch(removeFromFavorites(city));
-  },
+    addToFavorites: (objOfCity) => {
+        dispatch(addToFavorites(objOfCity));
+    },
+    removeFromFavorites: (city) => {
+        dispatch(removeFromFavorites(city));
+    },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(FavoriteCityButton);
