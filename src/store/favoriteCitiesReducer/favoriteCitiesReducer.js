c997a49eb6854c0a6360d05d66ef4eecf66d31c7
evergreen <dany2zverev@gmail.com>
2021-10-22 13:17:56 +0300

    Added a new action to update the date in city objects

--- src/store/favoriteCitiesReducer/favoriteCitiesReducer.js
+++ src/store/favoriteCitiesReducer/favoriteCitiesReducer.js
@@ -1,9 +1,11 @@
 import store from "../store";
-import {ADD_TO_FAVORITES, GET_CITIES, REMOVE_FROM_FAVORITES} from "./actionTypes";
+import {ADD_TO_FAVORITES, GET_CITIES, REMOVE_FROM_FAVORITES, UPDATE_TEMP} from "./actionTypes";
+import fetchData from "../../helpers/fetchData";
 
 export const addCity = (city) => ({type: ADD_TO_FAVORITES, payload: city})
 export const removeCity = (city) => ({type: REMOVE_FROM_FAVORITES, payload: city})
 export const getCitiesFromLocaleStorage = (cities) => ({type: GET_CITIES, payload: cities})
+export const updateTemp = (data) => ({type: UPDATE_TEMP, payload: data})
 
 export const citiesState = {
     arrOfCities: [],
@@ -12,11 +14,12 @@ export const citiesState = {
 export function favoriteCitiesReducer(state = citiesState, action) {
     switch (action.type) {
         case ADD_TO_FAVORITES:
-            return {...state,
+            return {
+                ...state,
                 arrOfCities: [...state.arrOfCities, {
                     city: action.payload.city,
-                    temp_c: action.payload.celsius,
-                    temp_f: action.payload.fahrenheit,
+                    temp_c: action.payload.temp_c,
+                    temp_f: action.payload.temp_f,
                     lastUpdated: action.payload.lastUpdated,
                 }]
             };
@@ -32,6 +35,15 @@ export function favoriteCitiesReducer(state = citiesState, action) {
                 ...state,
                 arrOfCities: action.payload,
             };
+        case UPDATE_TEMP:
+            return {
+                ...state,
+                arrOfCities: state.arrOfCities.map((obj) => obj.city === action.payload.location.name.toLowerCase() ? {
+                    ...obj, temp_c: action.payload.current.temp_c.toFixed(),
+                    temp_f: action.payload.current.temp_f.toFixed(),
+                    lastUpdated: Date.now(),
+                } : obj)
+            }
         default:
             return state;
     }
@@ -43,3 +55,12 @@ export const addToFavorites = (city) => (dispatch) => {
 export const removeFromFavorites = (city) => (dispatch) => {
     store.dispatch(removeCity(city));
 };
+
+export const fetchCityTemp = (city) => async (dispatch) => {
+    try {
+        const data = await (fetchData(city));
+        store.dispatch(updateTemp(data));
+    } catch (e) {
+        console.error(e);
+    }
+}
