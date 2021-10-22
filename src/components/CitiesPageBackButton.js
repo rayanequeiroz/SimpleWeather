473f5da5852495339a1bf8109c8ca03fc5357cbe
evergreen <dany2zverev@gmail.com>
2021-10-22 17:37:25 +0300

    refactoring of the paths for BackButton

--- src/components/CitiesPageBackButton.js
+++ src/components/CitiesPageBackButton.js
@@ -1,11 +1,11 @@
 import React from "react";
-import BackButton from "./UI/BackButton";
+import IconBackButton from "./UI/IconBackButton";
 import {Link} from "react-router-dom";
 
 const CitiesPageBackButton = () => {
     return (
         <Link to='/SimpleWeather' className='cities-page__back-btn'>
-            <BackButton
+            <IconBackButton
                 sx={{color: '#2D81FF', '&.MuiIconButton-root:hover': {backgroundColor: 'rgba(45, 129, 255, 0.02)'}}}/>
         </Link>
     );
