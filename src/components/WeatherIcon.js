8012efb089957a88d9c76abb3d050fe26d94beb5
evergreen <dany2zverev@gmail.com>
2021-10-26 23:47:18 +0300

    Icons have been moved to symbols for the ability to dynamically control their color

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import "../styles/CurrentForecast.css";
+import day from '../day-sprites.svg';
+import night from '../night-sprites.svg';
 
 const WeatherIcon = (props) => {
-    let timeOfDay = 'day';
-    if (!props.isDay) {
-        timeOfDay = 'night';
+    const hrefToIcon = `${props.isDay ? day : night}#${props.code}`;
+    const color = {
+        fill: '#fff'
     }
-
-    const weatherSrc = `${process.env.PUBLIC_URL}/assets/icons/${timeOfDay}/${props.code}.svg`;
-
     return (
         <div className='weather__icon'>
-            <img src={weatherSrc} className='weather__icon' alt='icon of weather'/>
+            <svg className='weather__icon' xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={color}>
+                <use href={hrefToIcon}></use>
+            </svg>
         </div>
     )
 
