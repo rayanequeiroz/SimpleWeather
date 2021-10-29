19b9dd8de8e5b35967f2e624d474e8b1fef1dee3
evergreen <dany2zverev@gmail.com>
2021-10-29 15:39:47 +0300

    Add swiper with forecast and headers

--- src/pages/ForecastPage.js
+++ src/pages/ForecastPage.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import ForecastPageBackButton from "../components/ForecastPageBackButton";
-import ForecastDay from "../components/ForecastDay";
+import ForecastSwiper from "../components/ForecastSwiper";
 import ForecastToggle from "../components/ForecastToggle";
+import {connect} from "react-redux";
 
-const ForecastPage = () => {
+const ForecastPage = (props) => {
+
     return (
         <div className='app app-outlined'>
             <Link to='/SimpleWeather/'>
                 <ForecastPageBackButton>Back to home page</ForecastPageBackButton>
             </Link>
-            <ForecastDay className='forecast-graph'/>
+            <h1 style={{fontWeight: 'normal', fontSize: '30px', marginTop: '10px', paddingTop: '5px', borderTop: '1px solid #F9FBFF'}}>{props.location}</h1>
+            <h2 style={{fontWeight: 'normal', fontSize: '20px', marginTop: '5px'}}>{props.country}</h2>
+            <ForecastSwiper/>
             <ForecastToggle/>
         </div>
     );
 };
 
-export default ForecastPage;
+const MapStateToProps = (state) => ({
+    location: state.weatherData.responseLocation,
+    country: state.weatherData.country,
+})
+export default connect(MapStateToProps)(ForecastPage);
