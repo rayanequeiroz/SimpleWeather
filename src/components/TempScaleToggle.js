2b590f79f0d907ec135aae4c00e337f95d0876aa
evergreen <dany2zverev@gmail.com>
2021-10-29 15:32:04 +0300

    refactor code for better readability

--- src/components/TempScaleToggle.js
+++ src/components/TempScaleToggle.js
@@ -9,71 +9,47 @@ const TempScaleToggle = (props) => {
     }
     const [value, setValue] = useState(localStorage.getItem('tempScale'));
     return (
-            <ToggleButtonGroup
-                color='secondary'
-                value={value}
-                exclusive
-                fullWidth={true}
-                sx={{height: '20px'}}
-                onChange={() => {
-                    if (value === 'celsius') {
-                        setValue('fahrenheit')
-                        props.changeTempScale('fahrenheit');
-                        localStorage.setItem('tempScale', 'fahrenheit')
-                    } else {
-                        setValue('celsius')
-                        props.changeTempScale('celsius')
-                        localStorage.setItem('tempScale', 'celsius')
+        <ToggleButtonGroup
+            color='secondary'
+            value={value}
+            exclusive
+            fullWidth={true}
+            sx={{
+                '& .MuiToggleButton-root': {
+                    backgroundColor: 'transparent',
+                    borderColor: '#F9FBFF',
+                    color: '#2d81ff',
+                    textTransform: 'capitalize',
+                    '&.Mui-selected': {
+                        backgroundColor: '#6BA6FF',
+                        color: '#ffffff',
+                    },
+                    '&.Mui-selected:hover': {
+                        backgroundColor: '#5096ff',
+                        color: '#ffffff',
                     }
-                }}
-            >
-                <ToggleButton
-                    value='fahrenheit'
-                    sx={{
-                        '&.MuiToggleButton-root': {
-                            borderRadius: '20px',
-                            backgroundColor: 'transparent',
-                            borderColor: '#F9FBFF',
-                            color: '#2d81ff',
-                            textTransform: 'capitalize',
-                        },
-                        '&.Mui-selected': {
-                            backgroundColor: '#6BA6FF',
-                            borderColor: '#6BA6FF',
-                            color: '#ffffff',
-                        },
-                        '&.Mui-selected:hover': {
-                            backgroundColor: '#5096ff',
-                            color: '#ffffff',
-                        },
-                    }}
-                >
-                    Fahrenheit
-                </ToggleButton>
-                <ToggleButton
-                    value='celsius'
-                    sx={{
-                        '&.MuiToggleButton-root': {
-                            borderRadius: '20px',
-                            backgroundColor: 'transparent',
-                            borderColor: '#F9FBFF',
-                            color: '#2d81ff',
-                            textTransform: 'capitalize',
-                        },
-                        '&.Mui-selected': {
-                            backgroundColor: '#6BA6FF',
-                            borderColor: '#6BA6FF',
-                            color: '#ffffff',
-                        },
-                        '&.Mui-selected:hover': {
-                            backgroundColor: '#5096ff',
-                            color: '#ffffff',
-                        },
-                    }}
-                >
-                    Celsius
-                </ToggleButton>
-            </ToggleButtonGroup>
+                },
+                height: '24px',
+            }}
+            onChange={() => {
+                if (value === 'celsius') {
+                    setValue('fahrenheit')
+                    props.changeTempScale('fahrenheit');
+                    localStorage.setItem('tempScale', 'fahrenheit')
+                } else {
+                    setValue('celsius')
+                    props.changeTempScale('celsius')
+                    localStorage.setItem('tempScale', 'celsius')
+                }
+            }}
+        >
+            <ToggleButton value='fahrenheit' sx={{borderRadius: '20px 0 0 20px'}}>
+                Fahrenheit
+            </ToggleButton>
+            <ToggleButton value='celsius' sx={{borderRadius: '0 20px 20px 0'}}>
+                Celsius
+            </ToggleButton>
+        </ToggleButtonGroup>
     );
 };
 
